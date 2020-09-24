dd63bb769d40dc8a24bbfd46d4797ee20653a30e
zhouxingu <zhouxinguo@itcast.cn>
2020-09-24 23:04:18 +0800

    模板2

---
+++ bookmanager02/book/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from book.models import BookInfo
+
+def create_book(request):
+    book = BookInfo.objects.create(
+        name = 'abc',
+        pub_date = '2000-1-1',
+        readcount=10
+    )
+
+    return HttpResponse('create')
+
+
+
+
+
