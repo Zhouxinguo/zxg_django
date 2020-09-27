4458c08bfde3da01ef9799c45a0dfc649f20971b
zhouxingu <zhouxinguo@itcast.cn>
2020-09-27 21:40:59 +0800

    Vue基本使用

--- bookmanager4/book/middleware.py
+++ bookmanager4/book/middleware.py
@@ -19,7 +19,7 @@ class TestMiddleware2(MiddlewareMixin):
     def process_request(self,request):
         print('process_request2 被调用')
 
-    def peocess_view(self,request,view_func,view_args,view_kwargs):
+    def process_view(self,request,view_func,view_args,view_kwargs):
         print('process_view2 被调用')
 
     def process_response(self,request,response):
